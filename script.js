565a9dc6af8ee446b5c0f2ae44b5321d6f31342b
Savio Shaji <savioshaji02@gmail.com>
2024-08-05 21:47:58 +0530

    Final changes

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 async function getQuote() {
   const response = await fetch("https://api.adviceslip.com/advice");
   const data = await response.json();
-  console.log(`${data.slip.advice}`);
+  const box = document.getElementsByClassName("quote")[0];
+  box.innerHTML = data.slip.advice;
 }
