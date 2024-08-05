fcb3eab58878593d6071495d4aaa5b034311e659
Savio Shaji <savioshaji02@gmail.com>
2024-08-05 21:25:29 +0530

    added changes

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 async function getQuote() {
-  const response = await fetch(
-    "https://evilinsult.com/generate_insult.php?lang=en&type=json"
-  );
+  const response = await fetch("https://api.adviceslip.com/advice");
   const data = await response.json();
-  console.log(`${data}`);
+  console.log(`${data.slip.advice}`);
 }
